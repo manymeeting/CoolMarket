7d3a318a5444a3f6cb5bd80d9b94efd143d52b06
Mokuten OH <mokuteno@gmail.com>
2017-12-06 14:01:54 -0800

    add dbconn and UserForm (as an example)

---
+++ dbconn/dbconn.php
@@ -0,0 +1,42 @@
+<?php
+// Extend this class to re-use db connection
+class DbConn
+{
+    /*
+    sample dbconf.php
+    //DATABASE CONNECTION VARIABLES
+    $host = "localhost"; // Host name
+    $username = "user"; // Mysql username
+    $password = "password"; // Mysql password
+    $db_name = "login"; // Database name
+
+    //DO NOT CHANGE BELOW THIS LINE UNLESS YOU CHANGE THE NAMES OF THE MEMBERS AND LOGINATTEMPTS TABLES
+
+    $tbl_prefix = ""; //***PLANNED FEATURE, LEAVE VALUE BLANK FOR NOW*** Prefix for all database tables
+    $tbl_members = $tbl_prefix."members";
+    $tbl_attempts = $tbl_prefix."loginAttempts";
+    */
+    
+    public $conn;
+    public function __construct()
+    {
+        require dirname(__FILE__). '/' . 'dbconf.php';
+        $this->host = $host; // Host name
+        $this->username = $username; // Mysql username
+        $this->password = $password; // Mysql password
+        $this->db_name = $db_name; // Database name
+        $this->tbl_prefix = $tbl_prefix; // Prefix for all database tables
+        $this->tbl_members = $tbl_members;
+        $this->tbl_attempts = $tbl_attempts;
+
+        try {
+			// Connect to server and select database.
+			$this->conn = new PDO('mysql:host=' . $host . ';dbname=' . $db_name . ';charset=utf8', $username, $password);
+			$this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		} catch (\Exception $e) {
+			die('Database connection error');
+		}
+    }
+}
+
+?>
---
+++ forms/userform.php
@@ -0,0 +1,105 @@
+<?php
+class UserForm extends DbConn
+{
+
+    public function searchUser($searchBy, $searchValue)
+    {
+        try
+        {
+            $db = new DbConn;
+            $tbl_members = $db->tbl_members;
+            // prepare sql and bind parameters
+            $dbstr = "SELECT * FROM " . $tbl_members . " WHERE " . $searchBy . " LIKE " . ":value";
+            $stmt = $db->conn->prepare($dbstr);
+            $searchValue = '%' . $searchValue . '%';
+            $stmt->bindParam(':value', $searchValue);
+            
+            $stmt->execute();
+            $result = $stmt->fetchAll();
+
+            $err = '';
+        }
+        catch (PDOException $e)
+        {
+            $err = "Error: " . $e->getMessage();
+            $result = null;
+        }
+
+        return $this->_ret($err, $result);   
+    }
+
+    public function fetchAllUser()
+    {
+        try
+        {
+            $db = new DbConn;
+            $tbl_members = $db->tbl_members;
+            // prepare sql and bind parameters
+            $dbstr = "SELECT * FROM " . $tbl_members;
+            $result = $db->conn->query($dbstr);
+
+            $err = '';
+        }
+        catch (PDOException $e)
+        {
+            $err = "Error: " . $e->getMessage();
+            $result = null;
+        }
+
+        return $this->_ret($err, $result);
+        
+    }
+
+    public function createUser($usr, $email, $pw, $firstname, $lastname, $homephone, $homeaddr, $cellphone)
+    {
+        try {
+
+            $db = new DbConn;
+            $tbl_members = $db->tbl_members;
+            // prepare sql and bind parameters
+            $dbstr = "INSERT INTO " . $tbl_members . " (username, password, email, firstname, lastname, homephone, homeaddr, cellphone) VALUES (:username, :password, :email, :firstname, :lastname, :homephone, :homeaddr, :cellphone)";
+            
+            $stmt = $db->conn->prepare($dbstr);
+            $stmt->bindParam(':username', $usr);
+            $stmt->bindParam(':email', $email);
+            $stmt->bindParam(':password', $pw);
+            $stmt->bindParam(':firstname', $firstname);
+            $stmt->bindParam(':lastname', $lastname);
+            $stmt->bindParam(':homephone', $homephone);
+            $stmt->bindParam(':homeaddr', $homeaddr);
+            $stmt->bindParam(':cellphone', $cellphone);
+
+            $stmt->execute();
+
+            $err = '';
+
+        } catch (PDOException $e) {
+
+            $err = "Error: " . $e->getMessage();
+
+        }
+        return $this->_ret($err, null);
+
+    }
+
+    private function _ret($err, $result)
+    {
+        //Determines returned value ('true' or error code)
+        if ($err == '') {
+
+            if(!is_null($result))
+            {
+                return $result;
+            }
+
+            $success = 'true';
+
+        } else {
+
+            $success = $err;
+
+        };
+
+        return $success;
+    }
+}
